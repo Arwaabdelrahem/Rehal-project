25eaccb60571c7c5bbcaf5962a0518c8edbfa252
ArwaAbdelrahem <arwaabdelrahem22@gmail.com>
2021-04-07 23:36:01 +0200

    adding sendCode, forgetPassword,changePassword & fetch user's profile

--- controllers/Users.js
+++ controllers/Users.js
@@ -5,6 +5,44 @@ const nodemailer = require("nodemailer");
 const CodeGenerator = require("node-code-generator");
 const fs = require("fs");
 
+exports.profile = async (req, res, next) => {
+  const user = await User.findOne({ email: req.user.email });
+  res.status(200).send(user);
+};
+
+exports.sendCode = async (req, res, next) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(404).send("Invalid email");
+
+  var generator = new CodeGenerator();
+  const code = generator.generateCodes("#+#+#+", 100)[0];
+
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "arwaabdelrahem2@gmail.com",
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  var mailOptions = {
+    from: "arwaabdelrahem2@gmail.com",
+    to: req.body.email,
+    subject: "Verfication Code",
+    text: `Reset password code ${code}`,
+  };
+
+  transporter.sendMail(mailOptions, async function (error, info) {
+    if (error) {
+      res.status(400).send(error);
+    } else {
+      req.body.codeVerifing = code;
+      await user.set(req.body).save();
+      res.status(200).send(`Email sent: ${info.response}`);
+    }
+  });
+};
+
 exports.Register = async (req, res, next) => {
   const { error } = register(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -46,7 +84,7 @@ exports.Register = async (req, res, next) => {
     }
   });
 
-  req.body.emailVerifingCode = code;
+  req.body.codeVerifing = code;
   user = new User(req.body);
   await user.save();
 
@@ -54,13 +92,13 @@ exports.Register = async (req, res, next) => {
   res.status(201).send(user);
 };
 
-exports.verifyEmail = async (req, res, next) => {
+exports.verifyCode = async (req, res, next) => {
   let user = await User.findOne({ email: req.body.email });
   if (!user) return res.status(400).send("please enter a valid email");
 
   try {
-    if (user.emailVerifingCode === req.body.code) {
-      user.emailVerifingCode = "";
+    if (user.codeVerifing === req.body.code) {
+      user.codeVerifing = "";
       user = await user.save();
       res.status(200).send(user);
     }
@@ -81,4 +119,34 @@ exports.LogIn = async (req, res, next) => {
 
   const token = user.generateToken();
   res.status(200).send({ user, token });
+};
+
+exports.forgetPassword = async (req, res, next) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(404).send("Invalid email");
+
+  try {
+    if (user.codeVerifing === req.body.code) {
+      user.password = await bcrypt.hash(req.body.newPassword, 10);
+      user.codeVerifing = "";
+      user = await user.save();
+      res.status(200).send({ msg: "Code sent", user });
+    }
+  } catch (error) {
+    console.log("catch");
+    res.status(400).send(error.message);
+  }
+};
+
+exports.changePassword = async (req, res, next) => {
+  const compare = await bcrypt.compare(req.body.oldPassword, req.user.password);
+  if (!compare) return res.status(400).send("Incorrect password");
+
+  try {
+    req.user.password = await bcrypt.hash(req.body.newPassword, 10);
+    req.user = await req.user.save();
+    res.status(200).send(req.user);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const userController = require("../controllers/Users");
 const multer = require("../middlewares/multer");
+const auth = require("../middlewares/auth");
 const router = express.Router();
 
+router.get("/profile", auth, userController.profile);
+
 router.post("/register", multer, userController.Register);
-router.post("/verifyEmail", userController.verifyEmail);
+router.post("/sendCode", userController.sendCode);
+router.post("/verifyEmail", userController.verifyCode);
+router.post("/forgetPassword", userController.forgetPassword);
+router.post("/changePassword", auth, userController.changePassword);
 router.post("/login", userController.LogIn);
 
 module.exports = router;
