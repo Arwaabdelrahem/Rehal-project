6266c9df66961b646568b689fc6d7a5cfc3996c6
Arwa Abdelrahem <arwaabdelrahem22@gmail.com>
2021-07-25 02:19:43 +0200

    make getAll & getById public

--- routes/booking.js
+++ routes/booking.js
@@ -3,10 +3,10 @@ const bookingcntrl = require("../controllers/Bookings");
 const auth = require("../middlewares/auth");
 const router = express.Router();
 
-router.use(auth);
-
 router.get("/", bookingcntrl.getAll);
 router.get("/:bookingId", bookingcntrl.getById);
+
+router.use(auth);
 router.post("/newBooking/:placeId", bookingcntrl.newBooking);
 router.put("/edit/:bookingId", bookingcntrl.editBooking);
 router.delete("/delete/:bookingId", bookingcntrl.deleteBooking);
